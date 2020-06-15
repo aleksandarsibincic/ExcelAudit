ebacaf991ce4c9f8e8f4803bd34fd83fc7b81bb8
Aleksandar Sibincic <asibincic@netconomy.net>
2020-06-15 23:51:18 +0200

    reading ontology model with jena

--- src/main/java/ExcelAudit.java
+++ src/main/java/ExcelAudit.java
@@ -1,13 +1,26 @@
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+import java.io.File;
 import java.io.IOException;
 
 public class ExcelAudit {
 
     public static final String SAMPLE_XLSX_FILE_PATH = "./sample_file.xlsx";
+    public static final String ONTOLOGY_PATH = "./excel_ontology.owl" ;
 
     public static void main(String[] args) throws IOException {
 
+
+
         ExcelReader excelReader = new ExcelReader();
         excelReader.readFile(SAMPLE_XLSX_FILE_PATH);
+
+        Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+        File ontologyFile = new File(ONTOLOGY_PATH);
+        model.read(ontologyFile.toURL().toString());
+        model.write(System.out);
 
     }
 }
