33fbef58d783196481f4e9a12ce6ed43d7de821e
Aleksandar Sibincic <asibincic@netconomy.net>
2020-06-18 01:13:16 +0200

    created in relation

--- src/main/java/ExcelAudit.java
+++ src/main/java/ExcelAudit.java
@@ -7,8 +7,16 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.tdb.TDB;
 import org.apache.jena.tdb.TDBFactory;
+import org.apache.poi.hssf.usermodel.HSSFEvaluationWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.formula.FormulaParser;
+import org.apache.poi.ss.formula.FormulaType;
+import org.apache.poi.ss.formula.ptg.Ptg;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFEvaluationWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.eclipse.rdf4j.model.impl.TreeModel;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.manager.LocalRepositoryManager;
@@ -16,6 +24,7 @@ import org.eclipse.rdf4j.repository.manager.RepositoryManager;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
@@ -29,7 +38,7 @@ public class ExcelAudit {
     public static final String ONTOLOGY_PATH = "./excel_ontology.owl";
     public static final String NAMESPACE = "http://www.semanticweb.org/ds/ontologies/2020/5/excel-audit#";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InvalidFormatException {
 
         /*
         prvi dio je vise vjezbanje da li radi excel
@@ -53,6 +62,9 @@ public class ExcelAudit {
             dataFormatter.printCellValue(cell);
             System.out.println();
         });
+
+        XSSFWorkbook wb = new XSSFWorkbook(new File(SAMPLE_XLSX_FILE_PATH));
+        XSSFEvaluationWorkbook xssfew = XSSFEvaluationWorkbook.create(wb);
 
         /* Create Ontology model */
         OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
@@ -79,6 +91,8 @@ public class ExcelAudit {
         ObjectProperty hasCell = model.getObjectProperty(NAMESPACE + "hasCell");
         /*get cell value property*/
         DatatypeProperty value = model.getDatatypeProperty(NAMESPACE + "value");
+        /*get cell relations*/
+        ObjectProperty in = model.getObjectProperty(NAMESPACE + "in");
         sheets.forEach(sheet -> {
             /* create spreadsheet individual*/
             Individual spreadsheetIndividual = spreadsheetClass.createIndividual(NAMESPACE + sheet.getSheetName());
@@ -87,22 +101,32 @@ public class ExcelAudit {
             /* get all cells from sheet */
             List<Cell> sheetCells = excelReader.getCells(sheet);
             sheetCells.forEach(cell -> {
-                /* create cell individual*/
-                Individual cellIndividual = cellClass.createIndividual(NAMESPACE + cell.getAddress().toString());
-                /* add relations between sheet and cells*/
-                model.add(spreadsheetIndividual, hasCell, cellIndividual);
-                switch (cell.getCellTypeEnum()) {
-                    case NUMERIC:
-                        if (!DateUtil.isCellDateFormatted(cell)) {
-                            double cellValue = cell.getNumericCellValue();
-                            cellIndividual.addProperty(value, String.valueOf(cellValue));
+                if (cell.getCellTypeEnum().equals(CellType.NUMERIC) && !DateUtil.isCellDateFormatted(cell)) {
+                    /* create cell individual*/
+                    Individual cellIndividual = cellClass.createIndividual(NAMESPACE + cell.getAddress().toString());
+                    /* add relations between sheet and cells*/
+                    model.add(spreadsheetIndividual, hasCell, cellIndividual);
+                    //add numerical value to cell
+                    double cellValue = cell.getNumericCellValue();
+                    cellIndividual.addProperty(value, String.valueOf(cellValue));
+                }
+            });
+            sheetCells.forEach(cell -> {
+                if (cell.getCellTypeEnum().equals(CellType.FORMULA)) {
+                    Individual cellIndividual = cellClass.createIndividual(NAMESPACE + cell.getAddress().toString());
+                    /* add relations between sheet and cells*/
+                    model.add(spreadsheetIndividual, hasCell, cellIndividual);
+                    Ptg[] ptg = FormulaParser.parse(cell.getCellFormula(), xssfew, FormulaType.NAMEDRANGE, sheets.indexOf(sheet), cell.getRowIndex());
+                    for (int i = 0; i < ptg.length; i++) {
+                        byte cla = ptg[i].getPtgClass();
+                        if (cla == Ptg.CLASS_VALUE) {
+                            String index = ptg[i].toFormulaString();
+                            Individual individualCell = model.getIndividual(NAMESPACE+index);
+                            System.out.println(in);
+                            model.add(individualCell, in, cellIndividual);
+
                         }
-                        break;
-                    case FORMULA:
-                        String formula = cell.getCellFormula();
-                        /* maybe to use org.apache.poi.ss.formula.FormulaParser for this */
-                    default:
-                        //do nothing
+                    }
                 }
             });
         });
